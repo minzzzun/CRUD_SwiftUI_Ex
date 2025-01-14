7df004209ee8b128366820d62194ad08f7dcfe7d
minjun kim <minzzzun@gmail.com>
2025-01-15 00:21:43 +0900

    1/14 여기까지

--- CRUD_SwiftUI_Ex/ContentView.swift
+++ CRUD_SwiftUI_Ex/ContentView.swift
@@ -3,29 +3,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
-        NavigationView {
-            List{
-                ForEach(1...10,id: \.self){_ in
-                    CellView()
+    
+            NavigationView {
+                List{
+                    ForEach(1...10,id: \.self){_ in
+                        CellView()
+                    }
                 }
-            }
-            .listStyle(PlainListStyle())
-            .navigationTitle("Title")
-            .navigationBarItems(
-                    trailing: NavigationLink(destination:
-                            AddView(),
-                                label:{
-                                     Text("추가하기")
-                                        .fontWeight(.bold)
-                                        .foregroundColor(Color.init(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
-                                 }
-                  )
-                )
-            
-            
-            
-        }//nav
+                .listStyle(PlainListStyle())
+                .navigationTitle("Title")
+                .navigationBarItems(
+                        trailing: NavigationLink(destination:
+                                AddView(),
+                                    label:{
+                                         Text("추가하기")
+                                            .fontWeight(.bold)
+                                            .foregroundColor(Color.init(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
+                                     }
+                      )
+                    )
+            }//nav
+            .background(Color.yellow)
+            .edgesIgnoringSafeArea(.all)
+        
+        
         
         
         
